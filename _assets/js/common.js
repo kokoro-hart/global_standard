415e196499164cede4e4d43142675c6219c6dd81
kokoro <cocosyu.11261126@gmail.com>
2021-10-07 23:25:04 +0900

    Fix: ドロワーボタンはクラスで操作する

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -36,10 +36,12 @@ jsDrawerButton.addEventListener('click', function () {
   body.classList.toggle('is-drawerActive')
   if (jsDrawerButton.getAttribute('aria-expanded') == 'false') {
     this.setAttribute('aria-expanded', 'true');
+    this.classList.add('is-active');
     spGlobalNav.setAttribute('area-hidden', 'false');
     spGlobalNav.classList.add('is-active');
   } else {
-    this.setAttribute('aria-expanded', 'false')
+    this.setAttribute('aria-expanded', 'false');
+    this.classList.remove('is-active');
     spGlobalNav.setAttribute('area-hidden', 'true');
     spGlobalNav.classList.remove('is-active');
   };
