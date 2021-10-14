527b7ebc9f511d9a1a5f85e6bed28a1e90d4fd9a
kokoro <cocosyu.11261126@gmail.com>
2021-10-14 22:43:45 +0900

    Add: footer.phpを追加

---
+++ wp/footer.php
@@ -0,0 +1,55 @@
+    <!--フッター-->
+    <!--お問い合わせ　資料ダウンロード-->
+    <div class="c-footer-links">
+      <div class="c-footer-links__item">
+        <div class="c-footer-links__item-inner">
+          <h2 class="c-footer-links__title-head u-font-italic">DOWNLOAD</h2>
+          <p class="c-footer-links__title-foot">資料ダウンロード</p>
+          <a href="/download/" class="c-footer-links__button u-font-italic">
+            View more
+            <svg class="c-svg c-footer-links__svg" width="20" height="13.5">
+              <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#icon-arrow02" />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="c-footer-links__item">
+        <div class="c-footer-links__item-inner">
+          <h2 class="c-footer-links__title-head u-font-italic">CONTACT</h2>
+          <p class="c-footer-links__title-foot">お問い合わせ</p>
+          <a href="/contact/" class="c-footer-links__button u-font-italic">
+            View more
+            <svg class="c-svg c-footer-links__svg" width="20" height="13.5">
+              <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#icon-arrow02" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!--お問い合わせ　資料ダウンロード-->
+    <footer class="l-footer">
+      <div class="l-footer__inner">
+        <h1 class="l-footer-logo">
+          <a class="l-footer-logo__link" href="/">
+            Global Standard
+          </a>
+        </h1>
+        <div class="l-footer__info">
+          <p class="l-footer__text">
+            〒550-1000　大阪市西区土佐堀9-5-5 <br>
+            TEL　06-123-4567 <br>
+            FAX　06-123-4568
+          </p>
+        </div>
+        <div class="l-footer__copyright">
+          <span lang="en">©︎2021 Global Standard. All Rights Reserved.</span>
+        </div>
+      </div>
+    </footer>
+    <!--フッター-->
+  </div>
+  <!-- /wrapper -->
+  
+  <?php wp_footer(); ?>
+</body>
+</html>
