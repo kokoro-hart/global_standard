e81b554b65ae4c880bcdf71211ff8b9595ad76c3
kokoro <cocosyu.11261126@gmail.com>
2021-10-15 23:36:16 +0900

    Add: 投稿固定ページを追加

---
+++ wp/single.php
@@ -0,0 +1,94 @@
+    
+    <?php get_header(); ?>
+    
+    <div class="p-mv-lower p-mv-lower--bg-news">
+      <svg class="c-svg p-mv-lower__svg p-mv-lower__svg--sp u-hidden-md-up">
+        <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#mv-lower-decoration_sp" />
+      </svg>
+      <svg class="c-svg p-mv-lower__svg p-mv-lower__svg--pc u-hidden-md-down">
+        <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#mv-lower-decoration_pc" />
+      </svg>
+      <div class="p-mv-lower__heading">
+        <h2 class="p-mv-lower__heading-head u-font-italic">
+          NEWS
+        </h2>
+        <br>
+        <p class="p-mv-lower__heading-foot">
+          ニュース
+        </p>
+      </div>
+    </div>
+    <!--パンくずリスト-->
+    <div class="c-breadcrumb u-mt-12">
+      <div class="l-inner">
+        <ul class="c-breadcrumb__list" itemscope itemtype="https://schema.org/BreadcrumbList">
+          <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+            <a class="c-breadcrumb__link" itemprop="item" href="/"><span itemprop="name">ホーム</span></a>
+            <meta itemprop="position" content="1" />
+          </li>
+          <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+            <a class="c-breadcrumb__link" itemprop="item" href="/news/"><span itemprop="name">ニュース</span></a>
+            <meta itemprop="position" content="2" />
+          </li>
+          <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+            <a class="c-breadcrumb__link" itemprop="item" href="/news/single"><span itemprop="name">シングルページ</span></a>
+            <meta itemprop="position" content="3" />
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--/パンくずリスト-->
+    <section class="p-single l-archive">
+      <div class="l-archive__inner l-inner">
+        <!--メインエリア-->
+        <main class="p-single__main l-archive__main">
+          <div class="p-single__meta">
+            <span class="p-single__cat">カテゴリ</span>
+            <time class="p-single__time" datetime="2021.01.01">2021.01.01</time>
+          </div>
+          <h1 class="p-single__title">
+            常にハイパフォーマンスを目指すグローバルビジネスの現場を肌で感じることで、自走・自立できる思考力・適応力を醸成する起爆剤研修。
+          </h1>
+          <div class="p-single__thumbnail">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/common/icatch-news.jpeg" alt="記事の画像" class="p-single__img">
+          </div>
+          <article class="p-single__content">
+            <h2>H2見出しH2見出しH2見出し</h2>
+            <p>
+              ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。
+            </p>
+            <h3>H3見出しH3見出しH3見出し</h3>
+            <p>
+              ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。
+            </p>
+            <h4>H4見出しH4見出しH4見出し</h4>
+            <p>
+              ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。
+            </p>
+            <ul>
+              <li>・リスト</li>
+              <li>・リスト</li>
+              <li>・リスト</li>
+            </ul>
+            <blockquote class="wp-block-quote">
+              <p>引用した文章が入ります。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。</p>
+              <svg class="c-svg wp-block-quote__svg" width="8" height="14">
+                <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#wp-blockquote" />
+              </svg>
+            </blockquote>
+            <div class="p-single__post-links">
+              <div class="p-single__post-link"><a href="">
+                  < 前の記事へ</a>
+              </div>
+              <div class="p-single__post-link"><a href="">次の記事へ ></a></div>
+            </div>
+          </article>
+        </main>
+        <!--/メインエリア-->
+
+        <?php get_sidebar(); ?>
+
+      </div>
+    </section>
+    
+    <?php get_footer(); ?>
