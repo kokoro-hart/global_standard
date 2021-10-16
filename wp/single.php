48a1c1cba9aac2478959e1d2601bf6601f4b27c1
kokoro <cocosyu.11261126@gmail.com>
2021-10-16 20:04:00 +0900

    Fix: 投稿固定ページを動的に修正

--- wp/single.php
+++ wp/single.php
@@ -34,46 +34,53 @@
         <!--メインエリア-->
         <main class="p-single__main l-archive__main">
           <div class="p-single__meta">
-            <span class="p-single__cat">カテゴリ</span>
-            <time class="p-single__time" datetime="2021.01.01">2021.01.01</time>
+            <?php $category = get_the_category(); ?>
+            <?php if($category[0]) : ?>
+            <a href="<?php echo esc_url(get_category_link($category[0]->term_id)); ?>" class="p-single__cat">
+              <?php echo $category[0]->cat_name; ?>
+            </a>
+            <?php endif; ?>
+            <time class="p-single__time" datetime="<?php the_time(' c '); ?>"><?php the_time('Y.n.j'); ?></time>
           </div>
           <h1 class="p-single__title">
-            常にハイパフォーマンスを目指すグローバルビジネスの現場を肌で感じることで、自走・自立できる思考力・適応力を醸成する起爆剤研修。
+            <?php the_title(); ?>
           </h1>
           <div class="p-single__thumbnail">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/common/icatch-news.jpeg" alt="記事の画像" class="p-single__img">
+            <?php
+              if(has_post_thumbnail()) {
+                the_post_thumbnail('large' , array(
+                  'class' => 'p-single__img',
+                ));
+              } else {
+                echo '<img data-src="'. esc_url(get_template_directory_uri()) . '/img/common/icatch-news.jpeg" alt="記事の画像" class="p-single__img">';
+              }
+            ?>
           </div>
           <article class="p-single__content">
-            <h2>H2見出しH2見出しH2見出し</h2>
-            <p>
-              ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。
-            </p>
-            <h3>H3見出しH3見出しH3見出し</h3>
-            <p>
-              ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。
-            </p>
-            <h4>H4見出しH4見出しH4見出し</h4>
-            <p>
-              ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。
-            </p>
-            <ul>
-              <li>・リスト</li>
-              <li>・リスト</li>
-              <li>・リスト</li>
-            </ul>
-            <blockquote class="wp-block-quote">
-              <p>引用した文章が入ります。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。今後の目標はプロジェクトマネジメントに特化したマネージャーになることです。ビジネスパーソンとの交流の中で、より具体的に現地で働く人々の仕事に対する熱意や誇り、それぞれが持つリーダーシップなどを学ぶことができ非常に良い経験となりました。</p>
-              <svg class="c-svg wp-block-quote__svg" width="8" height="14">
-                <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#wp-blockquote" />
-              </svg>
-            </blockquote>
-            <div class="p-single__post-links">
-              <div class="p-single__post-link"><a href="">
-                  < 前の記事へ</a>
-              </div>
-              <div class="p-single__post-link"><a href="">次の記事へ ></a></div>
-            </div>
+            <?php the_content();?>
           </article>
+
+          <!--ページネーション-->
+          <?php
+            $prev_post = get_previous_post(); // 前の投稿を取得
+            $next_post = get_next_post(); // 次の投稿を取得
+          ?>
+          <div class="p-single__post-links">
+            <?php if ($prev_post) : ?>
+              <div class="p-single__post-link">
+                <?php previous_post_link('%link','< 前の記事へ'); ?>
+              </div>
+            <?php endif; ?>
+
+            <?php if ($next_post) : ?>
+              <div class="p-single__post-link">
+                <?php
+                  next_post_link('%link', '次の記事へ >');
+                ?>
+              </div>
+            <?php endif; ?>
+          </div>
+          <!--ページネーション-->
         </main>
         <!--/メインエリア-->
 
