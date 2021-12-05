48fee9d1016fcf768b55d0247681dac7977e697a
kokoro <cocosyu.11261126@gmail.com>
2021-12-05 17:39:44 +0900

    Fix: 変数名を修正 ナビゲーションへのsetAttributeを削除

--- _assets/js/modules/drawer.js
+++ _assets/js/modules/drawer.js
@@ -1,20 +1,18 @@
 
-//  ドロワーメニュー
-const jsDrawerButton = document.getElementById('js-drawer');
+// ドロワーメニュー
+const drawerButton = document.getElementById('js-drawer');
 const body = document.body;
-const spGlobalNav = document.getElementById('js-global-nav');
+const drawerNav = document.getElementById('js-global-nav');
 
-jsDrawerButton.addEventListener('click', function () {
+drawerButton.addEventListener('click', function () {
   body.classList.toggle('is-drawerActive')
-  if (jsDrawerButton.getAttribute('aria-expanded') == 'false') {
+  if (drawerButton.getAttribute('aria-expanded') == 'false') {
     this.setAttribute('aria-expanded', 'true');
     this.classList.add('is-active');
-    spGlobalNav.setAttribute('area-hidden', 'false');
-    spGlobalNav.classList.add('is-active');
+    drawerNav.classList.add('is-active');
   } else {
     this.setAttribute('aria-expanded', 'false');
     this.classList.remove('is-active');
-    spGlobalNav.setAttribute('area-hidden', 'true');
-    spGlobalNav.classList.remove('is-active');
+    drawerNav.classList.remove('is-active');
   };
 });
